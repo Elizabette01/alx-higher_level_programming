3c676a2ed12b23082966e4426076a890939ae11f
Elizabeth_Nduka <ndukaelizabeth26@gmail.com>
2022-12-20 15:47:54 -0800

    [Task 1]: Adding a class that defines a square by it's size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Class square that defines a square."""
+    def __init__(self, size):
+        """Initialize method that sores the size of a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
